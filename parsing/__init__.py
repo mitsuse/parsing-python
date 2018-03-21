c39b84d0a7635f79e6c7fb355dffb9aab9d1a2af
Tomoya Kose <tomoya@mitsuse.jp>
2018-03-22 00:15:52 +0900

    Define a protocol `DirectedGraph`.

--- parsing/__init__.py
+++ parsing/__init__.py
@@ -2,10 +2,14 @@
 # coding: utf-8
 
 from typing import Generic
+from typing import Set
 from typing import TypeVar
+from typing_extensions import Protocol
 
 
 A = TypeVar('A')
+V = TypeVar('V')
+L = TypeVar('L')
 
 
 class Word(Generic[A]):
@@ -38,3 +42,27 @@ class Token(Generic[A]):
     @property
     def index(self) -> int:
         return self.__index
+
+
+class DirectedGraph(Protocol[V, L]):
+    def vertices(self) -> Set[V]: ...
+    def edges(self) -> Set[DirectedEdge[V, L]]: ...
+
+
+class DirectedEdge(Generic[V, L]):
+    def __init__(self, start: V, end: V, label: L) -> None:
+        self.__start = start
+        self.__end = end
+        self.__label = label
+
+    @property
+    def start(self) -> V:
+        return self.__start
+
+    @property
+    def end(self) -> V:
+        return self.__end
+
+    @property
+    def label(self) -> L:
+        return self.__label
