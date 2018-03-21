0d26536a15346331d8adbf9638a9eb3e590779be
Tomoya Kose <tomoya@mitsuse.jp>
2018-03-21 17:26:04 +0900

    `Word` should have its `surface` at least.

--- parsing/__init__.py
+++ parsing/__init__.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
+
+from typing import List
+from typing import NewType
+from typing import TypeVar
+
+from typing_extensions import Protocol
+
+
+class Word(Protocol):
+    def surface(self) -> str:
+        raise NotImplementedError()
