f3cfbe2aeb068aca0eea2fca431d2fd8de191035
Tomoya Kose <tomoya@mitsuse.jp>
2018-03-26 17:38:50 +0900

    wip

--- parsing/transition.py
+++ parsing/transition.py
@@ -7,21 +7,17 @@ from typing import Tuple
 from typing import TypeVar
 from typing_extensions import Protocol
 
-import parsing 
+import parsing
 
 from parsing import ling
 from parsing import math
-
 
 A = TypeVar('A')
 L = TypeVar('L')
 
 
 class Parser(Generic[A, L]):
-    def parse(
-        self,
-        sentence: 'ling.Sentence[A]'
-    ) -> 'parsing.Graph[A, L]':
+    def parse(self, sentence: 'ling.Sentence[A]') -> 'parsing.Graph[A, L]':
         pass
 
 
@@ -29,24 +25,17 @@ class State(Generic[A, L]):
     @staticmethod
     def initial(sentence: 'ling.Sentence[A]') -> 'State[A, L]':
         queue: Tuple['ling.Token[A]', ...] = tuple(
-            map(
-                lambda index, word: ling.Token(word, index),
-                enumerate(sentence, start=1)
-            )
-        )
+            map(lambda index, word: ling.Token(word, index),
+                enumerate(sentence, start=1)))
         return State(
             # (ling.Token(ling.Root, 0)),
             (),
             queue,
-            frozenset()
-        )
+            frozenset())
 
-    def __init__(
-        self,
-        stack: Tuple['ling.Token[A]', ...],
-        queue: Tuple['ling.Token[A]', ...],
-        edges: FrozenSet['parsing.Edge[A, L]']
-    ) -> None:
+    def __init__(self, stack: Tuple['ling.Token[A]', ...],
+                 queue: Tuple['ling.Token[A]', ...],
+                 edges: FrozenSet['parsing.Edge[A, L]']) -> None:
         self.__stack = stack
         self.__queue = queue
         self.__edges = edges
